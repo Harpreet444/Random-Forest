3c9c0a1a60f8de6331e9d459622976a22a2e0187
Harpreet444 <145033818+Harpreet444@users.noreply.github.com>
2024-06-29 11:41:50 +0530

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -10,24 +10,24 @@ import numpy as np
 
 
 target_array = np.array(['setosa', 'versicolor', 'virginica'])
-st.set_page_config(layout='wide',page_title="Iris flower classification",page_icon='D:\\Machine_learning\\random_forest\\iris_petal_sepal.png')
+st.set_page_config(layout='wide',page_title="Iris flower classification",page_icon='iris_petal_sepal.png')
 
 st.markdown("<h1 style='text-align: center; color: #5A4FCF;'>Iris flower classification (Random Forest)</h1>", unsafe_allow_html=True)
 
 col1,col2 = st.columns([1,2])
-col1.image("D:\\Machine_learning\\random_forest\\Iris_petal_sepal.png")
+col1.image("Iris_petal_sepal.png")
 col2.subheader("The primary goal is to leverage machine learning techniques to build a classification model that can accurately identify the species of iris flowers based on their measurements. The model aims to automate the classification process, offering a practical solution for identifying iris species.")
 col2.subheader('Key Details:')
 col2.subheader('Iris flowers have three species: setosa, versicolor, and virginica.')
 col2.subheader('These species can be distinguished based on measurements such as sepal length, sepal width, petal length, and petal width.')
 
-df = pd.read_csv("log_img//df.csv")
+df = pd.read_csv("df.csv")
 df.drop(df.columns[0],axis='columns',inplace=True)
 
 st.markdown("<h1 style='text-align: center; color: #5A4FCF;'>Dataset Structure</h1>", unsafe_allow_html=True)
 st.table(df.head())
 
-model = joblib.load("D:\\Machine_learning\\random_forest\\model.joblib")
+model = joblib.load("model.joblib")
 plt.title("Confusion matrix heat map representation")
 
 x_train, x_test, y_train, y_test = train_test_split(df.drop(['target'],axis='columns'),df['target'],test_size=0.2,random_state=10)
